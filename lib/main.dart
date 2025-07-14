14b6d1dd4677d57f76ed4e6790fb49c5c505cab9
Dynamicphavy <favourajayi308@gmail.com>
2025-07-14 09:25:18 +0100

    sim

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,15 @@ import 'package:demoapp/pages/sam_page.dart';
 import 'package:flutter/material.dart';
 import 'package:demoapp/auth/login_page.dart';
 import 'package:demoapp/pages/settings_page.dart';
+import 'package:device_preview/device_preview.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    DevicePreview(
+      builder: (context) =>
+      MyApp()
+    )
+  );
 }
 
 class MyApp extends StatelessWidget {
